e88ae891f0e32fe95705a3c027f79c34b6af1058
yongilkim01 <yongil.k01@gmail.com>
2024-02-21 04:10:48 +0900

    refactor(context): refactoring context code

--- shader/simple.vs
+++ shader/simple.vs
@@ -3,6 +3,7 @@
 layout (location = 0) in vec3 a_pos;
 layout (location = 1) in vec3 a_color;
 out vec3 our_color;
+//uniform vec3 a_color;
 
 void main()
 {
--- src/context.cc
+++ src/context.cc
@@ -11,10 +11,10 @@ ContextUniquePtr Context::create()
 bool Context::init()
 {
     float vertex_data[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f
     };
 
     uint32_t indices[] = {
@@ -26,10 +26,11 @@ bool Context::init()
     m_vertex_layout = VertexLayout::create();
 
     // create vertex array object
-    m_vertex_buffer = Buffer::create_with_data(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertex_data, sizeof(float) * 12);
+    m_vertex_buffer = Buffer::create_with_data(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertex_data, sizeof(float) * 24);
 
     // set position attribute
-    m_vertex_layout->set_attrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertex_layout->set_attrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertex_layout->set_attrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
     // color attribute
     //glEnableVertexAttribArray(1);
     //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -49,6 +50,7 @@ bool Context::init()
     // create shader program
     m_shader_program = ShaderProgram::create({fragment_shader, vertex_shader});
 
+
     if(!vertex_shader || !fragment_shader) return false;
 
     SPDLOG_INFO("shader program id: {}", m_shader_program->get());
@@ -63,9 +65,10 @@ void Context::render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //float time_value = glfwGetTime();
-    //float green_value = (sin(time_value) / 2.0f) + 0.5f;
-    //int vertex_color_loc = glGetUniformLocation(m_shader_program->get(), "our_color");
+    float time_value = glfwGetTime();
+    float green_value = (sin(time_value) / 2.0f) + 0.5f;
+    //int vertex_color_loc = glGetUniformLocation(m_shader_program->get(), "a_color");
+    //glUniform3f(vertex_color_loc, green_value * green_value, 2.0f * green_value * (1.0f - time_value), (1.0f - time_value) * (1.0f - time_value));
 
     m_shader_program->use();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
